4ac477fe1400bb91f026374a22e82b81b6af0d9d
Raezil <kamil.mazurek319@gmail.com>
2024-09-17 18:03:49 +0200

    feat: client got struct and new funcs

--- client/client.go
+++ client/client.go
@@ -20,25 +20,39 @@ type RPCError struct {
 	Message string `json:"message"`
 }
 
-func main() {
-	url := "http://localhost:8080/rpc"
+type RPCClient struct {
+	url   string
+	funcs map[string]interface{}
+}
+
+func NewRPCClient(url string) *RPCClient {
+	return &RPCClient{
+		url: url,
+	}
+}
+
+func (r *RPCClient) Register(name string, f interface{}) {
+	if r.funcs == nil {
+		r.funcs = make(map[string]interface{})
+	}
+	r.funcs[name] = f
+}
+
+func (r *RPCClient) Call(args map[string]interface{}) (*RPCResponse, error) {
+	// Prepare the request payload
 
 	// Prepare the request payload
-	reqBody, err := json.Marshal(map[string]interface{}{
-		"method": "add",
-		"params": RPCArgs{"a": 1.22,
-			"b": 2.33},
-	})
+	reqBody, err := json.Marshal(args)
 	if err != nil {
 		fmt.Println("Error encoding request:", err)
-		return
+		return nil, err
 	}
 
 	// Send the request
-	resp, err := http.Post(url, "application/json", bytes.NewBuffer(reqBody))
+	resp, err := http.Post(r.url, "application/json", bytes.NewBuffer(reqBody))
 	if err != nil {
 		fmt.Println("Error sending request:", err)
-		return
+		return nil, err
 	}
 	defer resp.Body.Close()
 
@@ -46,14 +60,14 @@ func main() {
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("Error reading response:", err)
-		return
+		return nil, err
 	}
 
 	var rpcResp RPCResponse
 	err = json.Unmarshal(body, &rpcResp)
 	if err != nil {
 		fmt.Println("Error decoding response:", err)
-		return
+		return nil, err
 	}
 
 	if rpcResp.Error != nil {
@@ -61,4 +75,19 @@ func main() {
 	} else {
 		fmt.Printf("RPC Result: %+v\n", rpcResp.Result)
 	}
+	return &rpcResp, nil
+}
+
+func main() {
+	url := "http://localhost:8080/rpc"
+
+	rpc := NewRPCClient(url)
+
+	args := map[string]interface{}{
+		"method": "add",
+		"params": RPCArgs{"a": 1.22,
+			"b": 2.33},
+	}
+	rpc.Call(args)
+
 }
