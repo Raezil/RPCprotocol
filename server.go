69fd49ac7c2fb73b7cf3bfc884f42b02ef124f58
Raezil <kamil.mazurek319@gmail.com>
2024-09-17 11:44:40 +0200

    feat: some changes

--- server.go
+++ server.go
@@ -6,6 +6,20 @@ type RPCServer struct {
 	funcs map[string]interface{}
 }
 
+type RPCError struct {
+	Code    string
+	Message string
+}
+
+func (r *RPCError) Error() {
+	fmt.Errorf("Error code: %s, %s", r.Code, r.Message)
+}
+
+type RPCResponse struct {
+	Result any
+	Error  RPCError
+}
+
 func NewRPCServer() *RPCServer {
 	return &RPCServer{
 		funcs: make(map[string]interface{}),
@@ -22,14 +36,14 @@ func (rpc *RPCServer) Register(name string, f interface{}) {
 
 }
 
-func (rpc *RPCServer) Call(name string, args RPCArgs) error {
-	rpc.funcs[name].(func(RPCArgs) error)(args)
+func (rpc *RPCServer) Call(name string, args RPCArgs) any {
+	rpc.funcs[name].(func(RPCArgs) any)(args)
 	return nil
 }
 
 func main() {
 	rpc := NewRPCServer()
-	rpc.Register("test", func(args RPCArgs) error {
+	rpc.Register("test", func(args RPCArgs) any {
 		fmt.Println(args)
 		return nil
 	})
